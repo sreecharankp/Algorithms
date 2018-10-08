4a44112ae434a7ef8b6b3513b4a73a7533580731
sreecharanp <sreecharan.prabhakar@infinite.com>
2018-10-08 12:34:02 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BubbleSort {
+
+    /*
+    Array before sorting: 20 35 -15 7 55 1 -22
+    Array after sorting: -22 -15 1 7 20 35 55
+    Time complexity is: O(n ^ 2)
+    */
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the length of the array:");
+        int length = Integer.valueOf(reader.readLine());
+        System.out.println("Enter " + length + " Elements");
+        int[] sortArray = new int[length];
+        for (int i = 0; i < sortArray.length; i++) {
+            sortArray[i] = Integer.valueOf(reader.readLine());
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i : sortArray) {
+            builder.append(i + " ");
+        }
+        System.out.println("Array before sorting: " + builder);
+
+        for (int i = sortArray.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (sortArray[j] > sortArray[j + 1]) {
+                    swapElement(sortArray, j, j + 1);
+                }
+            }
+        }
+        StringBuilder builderSort = new StringBuilder();
+        for (int j : sortArray) {
+            builderSort.append(j + " ");
+        }
+        System.out.println("Array after sorting: " + builderSort);
+    }
+
+    private static void swapElement(int[] sortArray, int i, int i1) {
+        if (i == i1) {
+            return;
+        }
+
+        int temp = sortArray[i];
+        sortArray[i] = sortArray[i1];
+        sortArray[i1] = temp;
+    }
+}
